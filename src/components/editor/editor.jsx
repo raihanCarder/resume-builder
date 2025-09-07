9fd6da91be465d751d2af95964c4f1c0f32269ba
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2025-09-07 08:49:41 -0400

    Add Dropdowns in editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -7,11 +7,26 @@ import ExperienceSection from "./editorExperience";
 export default function Editor({ resumeData, setResumeData }) {
   return (
     <div className="editor">
-      <GeneralInfoSection data={resumeData} setResumeData={setResumeData} />
-      <LinkSection data={resumeData} setResumeData={setResumeData} />
-      <SkillSection data={resumeData} setResumeData={setResumeData} />
-      <EducationSection data={resumeData} setResumeData={setResumeData} />
-      <ExperienceSection data={resumeData} setResumeData={setResumeData} />
+      <details>
+        <summary>General Info</summary>
+        <GeneralInfoSection data={resumeData} setResumeData={setResumeData} />
+      </details>{" "}
+      <details>
+        <summary>Link Information</summary>
+        <LinkSection data={resumeData} setResumeData={setResumeData} />
+      </details>{" "}
+      <details>
+        <summary>Skills Information</summary>
+        <SkillSection data={resumeData} setResumeData={setResumeData} />
+      </details>{" "}
+      <details>
+        <summary>Education Information</summary>
+        <EducationSection data={resumeData} setResumeData={setResumeData} />
+      </details>{" "}
+      <details>
+        <summary>Experience Information</summary>
+        <ExperienceSection data={resumeData} setResumeData={setResumeData} />
+      </details>{" "}
     </div>
   );
 }
